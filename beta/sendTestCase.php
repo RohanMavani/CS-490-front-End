52b149cf8cac6a07618d0680836d475058d420ff
RohanMavani <rohkmavani61094@gmail.com>
2017-10-24 15:53:09 -0400

    Latest Code

--- beta/sendTestCase.php
+++ beta/sendTestCase.php
@@ -1,31 +1,39 @@
 <?php
+  session_start();
   
   $str_json = file_get_contents('php://input');
   
   $arrayJSON= json_decode(stripslashes($str_json), true);
+  
+  
+  $url = 'https://web.njit.edu/~kb295/cs490/beta/createTestCase.php';
+  
+  
+    $data = array(
+    'question_id'  => $arrayJSON[question_id],
+    'test_case' => $arrayJSON[test_case],
+    'test_case_answer' => $arrayJSON[test_case_answer]
+  );
 
-  $data = array(
-    'question_body'  => $arrayJSON[question_body],
-    'question_weight' => $arrayJSON[question_weight],
-);
-
-
-  //API Url
-  $url = 'https://web.njit.edu/~kb295/cs490/beta/createQuestion.php';
-   
+  
+ 
   //Initiate cURL.
   $ch = curl_init($url);
    
   //Encode the array into JSON.
   $jsonDataEncoded = json_encode($data);
+ 
+  //echo sizeof($arrayJSON);
    
   //Tell cURL that we want to send a POST request.
+  
+
   curl_setopt($ch, CURLOPT_POST, 1);
   
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
    
   //Attach our encoded JSON string to the POST fields.
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+  curl_setopt($ch, CURLOPT_POSTFIELDS,   $jsonDataEncoded);
    
   //Set the content type to application/json
   curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
